e85dcdcc752f406698cc49792a7e9ead6f3bbbba
LazzyBird <olgathek@gmail.com>
2023-10-25 15:57:45 +0300

    shifting image start

---
+++ tests/herokuapp_practice_/shifting_image.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require("@playwright/test");
+
+const taskURL =
+"https://the-internet.herokuapp.com/shifting_content/image?mode=random&pixel_shift=100";
+let page;
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+  
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(taskURL);
+  await page.waitForLoadState("load");
+});
