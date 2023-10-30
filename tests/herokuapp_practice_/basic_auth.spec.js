39999e8cbe95f80b666ec74d7d73d9c032ec5495
LazzyBird <olgathek@gmail.com>
2023-10-30 17:36:40 +0200

    basic auth done w/o prompt

--- tests/herokuapp_practice_/basic_auth.spec.js
+++ tests/herokuapp_practice_/basic_auth.spec.js
@@ -1,27 +1,46 @@
-import { test, expect } from '@playwright/test';
-let taskURL =
-    "https://the-internet.herokuapp.com/basic_auth";
+import { test, expect } from "@playwright/test";
+let taskURL = "https://the-internet.herokuapp.com/basic_auth";
+let taskURLwithCredentials =
+  "https://admin:admin@the-internet.herokuapp.com/basic_auth";
 
-
-test('response status code is 401 until authenticated', async ({ request }) => {
-    let response = await request.get(taskURL);
-    await expect(response.status()).toBe(401);
-})
-/*
-test('Valid credentials', async ({ page }) => {
-  page.on('dialog', async (dialog) => {
-    await dialog.accept({ username: 'admin', password: 'admin' });
-  })
-  await page.getByRole('button', { name: 'Sign in' }).click();
-  await expect(page.locator('p')).toHaveText('Congratulations! You must have the proper credentials.');
+test("response status code is 401 until authenticated", async ({ request }) => {
+  let response = await request.get(taskURL);
+  expect(response.status()).toBe(401);
 });
 
-test('Dismiss dialog', async ({ page }) => {
-  page.on('dialog', async (dialog) => {
-    await dialog.dismiss();
-  })
-  await page.getByRole('button', { name: 'Cancel' }).click();
-  await expect(response.statusCode().toBe(401));
-  await expect(response.body().toContainText('unauthorized'));
-})
-*/
+test("Valid credentials added in http credentials", async ({
+  page,
+  request
+}) => {
+  let response = await request.get(taskURLwithCredentials);
+  expect(response.status()).toBe(200);
+  await page.goto(taskURLwithCredentials);
+  await expect(page.locator("p")).toHaveText(
+    "Congratulations! You must have the proper credentials."
+  );
+});
+/*
+test("Dismiss dialog leads to 401", async ({ page }) => {
+    await page.goto(taskURL);
+  page.on("dialog", async (dialog) => {
+    if (dialog.type() === "prompt") {
+      await dialog.dismiss();
+    }
+  });
+  await expect(page.response().status().toBe(401));
+}); */
+test("Basic Auth Demo with valid credentials added in context", async ({
+  browser
+}) => {
+  const context = await browser.newContext({
+    httpCredentials: {
+      username: "admin",
+      password: "admin"
+    }
+  });
+  let response = await request.get(taskURLwithCredentials);
+expect(response.status()).toBe(200);
+  const page = await context.newPage();
+  await page.goto("taskURL");
+  await expect(page.locator("div.example>h3")).toHaveText("Basic Auth");
+});
