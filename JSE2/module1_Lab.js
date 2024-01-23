e246d53940ad43e1b27322a79c33ac12e8c23e98
LazzyBird <olgathek@gmail.com>
2024-01-23 15:24:03 +0200

    array lab created

--- JSE2/module1_Lab.js
+++ JSE2/module1_Lab.js
@@ -1,4 +1,61 @@
 // Code Challenge #1
 {
-    
+    let images = [
+        { title: "Mona Lisa",
+    artist: "Leonardo da Vinci",
+    dateOfCreation: "1503",
+        },
+        {
+    title: "The Last Supper",
+    artist: "Leonardo da Vinci",
+    dateOfCreation: "1495"
+        },
+        {
+    title: "Starry Night",
+    artist: "Vincent van Gogh",
+    dateOfCreation: "1889"
+        },
+        {
+    title: "The Scream",
+    artist: "Edvard Munch",
+    dateOfCreation: "1893"
+        },
+        {
+    title: "Guernica",
+    artist: "Pablo Picasso",
+    dateOfCreation: "1937"
+        },
+        {
+    title: "The Kiss",
+    artist: "Gustav Klimt",
+    dateOfCreation: "1907"
+        },
+        {
+    title: "Girl With a Pearl Earring",
+    artist: "Johannes Vermeer",
+    dateOfCreation: "1665"
+        },
+        {
+    title: "The Birth of Venus",
+    artist: "Sandro Boticelli",
+    dateOfCreation: "1485"
+        },
+        {
+    title: "Las Meninas",
+    artist: "Diego Velazques",
+    dateOfCreation: "1656"
+        },
+        {
+    title: "The Creation of Adam",
+    artist: "Michelangelo",
+    dateOfCreation: "1512"
 }
+];
+// properties:
+/*
+{
+    title: "string",
+    artist: "string",
+    dateOfCreation: "number"
+}
+*/
