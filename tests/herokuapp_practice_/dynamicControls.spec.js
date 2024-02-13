c2c47a90ed4aef58c2189fa370d1301b2d74722a
LazzyBird <olgathek@gmail.com>
2024-02-13 12:53:34 +0200

    dymanic content done

--- tests/herokuapp_practice_/dynamicControls.spec.js
+++ tests/herokuapp_practice_/dynamicControls.spec.js
@@ -13,19 +13,37 @@ test.afterAll(async () => {
 test.beforeEach("Open Dynamic Controls URL", async ({ page }) => {
   await page.goto(`${taskURL}`, { timeout: 30000 });
 });
-// сторінка завантажується з чекбоксом та кнопокою "Прибрати"
-// після кліку по неї чекбокс зникає, з'являється повідомлення "Очікуйте", потім напис "Воно зникло", напис на кнопці змінюється на "Додати"
-// чи треба все робити в одному тесті як е2е
+
 test("Remove button functionality", async ({ page }) => {
   await page.getByRole("button", { name: "Remove" }).click();
   await expect(
     page.locator('text="Wait for it..."').first().isVisible()
   ).resolves.toBe(true);
   await page.waitForSelector('button:visible:has-text("Add")');
-  await expect(
-    page.getByRole("button", { name: "Add" }).isVisible()
-  ).resolves.toBe(true);
   await expect(page.locator('text="It\'s gone!"').isVisible()).resolves.toBe(
     true
   );
+  await expect(page.getByRole("checkbox").isVisible()).resolves.toBe(false);
+});
+
+test("Add button functionality", async ({ page }) => {
+  await page.getByRole("button", { name: "Remove" }).click();
+  await page.waitForSelector('button:visible:has-text("Add")');
+  await page.getByRole("button", { name: "Add" }).click();
+  await page.waitForSelector('button:visible:has-text("Remove")');
+  await expect(page.getByRole("checkbox").isVisible()).resolves.toBe(true);
+});
+
+test("Enable button functionality", async ({ page }) => {
+  await page.getByRole("button", { name: "Enable" }).click();
+  await page.waitForSelector('button:visible:has-text("Disable")');
+  await expect(page.getByRole("textbox").isDisabled()).resolves.toBe(false);
+});
+
+test("Disable button functionality", async ({ page }) => {
+  await page.getByRole("button", { name: "Enable" }).click();
+  await page.waitForSelector('button:visible:has-text("Disable")');
+  await page.getByRole("button", { name: "Disable" }).click();
+  await page.waitForSelector('button:visible:has-text("Enable")');
+  await expect(page.getByRole("textbox").isDisabled()).resolves.toBe(true);
 });
