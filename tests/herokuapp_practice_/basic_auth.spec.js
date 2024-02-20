3ffa8a52975d5f36ea442ad5753d6add3a3dde66
LazzyBird <olgathek@gmail.com>
2024-02-20 16:48:19 +0200

    вмираю нічо не робе

--- tests/herokuapp_practice_/basic_auth.spec.js
+++ tests/herokuapp_practice_/basic_auth.spec.js
@@ -2,6 +2,10 @@ import { test, expect } from "@playwright/test";
 let taskURL = "https://the-internet.herokuapp.com/basic_auth";
 let taskURLwithCredentials =
   "https://admin:admin@the-internet.herokuapp.com/basic_auth";
+const credentials = {
+  username: "admin",
+  password: "admin"
+};
 
 test("response status code is 401 until authenticated", async ({ request }) => {
   let response = await request.get(taskURL);
@@ -54,11 +58,11 @@ test("Clicking cancel leads to 401", async ({ page }) => {
 test("entering credintials  in prompt field and clicking ok leads to 200", async ({
   page
 }) => {
-  await page.goto(taskURL);
   page.on("dialog", async (dialog) => {
-    await dialog.accept("admin", "admin"); 
+    await dialog.accept(credentials.username, credentials.password);
+    await page.goto(taskURL, { waitUntil: "load" });
   });
-  page.waitForTimeout(500);
+  // await page.goto(taskURL, { waitUntil: "load" });
   await expect(page.locator("p")).toHaveText(
     "Congratulations! You must have the proper credentials."
   ); /// ahahaha page is not loaded until prompt dialog is handled
