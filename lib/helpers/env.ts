625c5750acb26b4451dd73e593135758e93bd2dd
LazzyBird <olgathek@gmail.com>
2024-02-21 15:40:21 +0200

    env setup for herocuapp tests

--- lib/helpers/env.ts
+++
@@ -1,5 +0,0 @@
-export default class Env {
-    public static readonly URL = process.env.URL;
-    public static readonly ADMIN_NAME = process.env.ADMIN_NAME;
-    public static readonly ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
-  }
