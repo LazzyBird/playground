74f747515528147c638f7b88b73c9a1262ccf00c
LazzyBird <olgathek@gmail.com>
2024-02-28 17:01:48 +0200

    report gen for brokenImages

--- tests/broken_images1.spec.js
+++ tests/broken_images1.spec.js
@@ -1,23 +1,22 @@
 import { test, expect } from "@playwright/test";
 import Env from "@helpers/env";
-import checkAndListImages from "@datafactory";
+import { reportImages, checkImages } from "@datafactory/brokenImagesHelper";
 const taskURL = Env.URL + "broken_images";
 let page;
-
+//
 test.beforeAll(async ({ browser }) => {
   page = await browser.newPage();
 });
-
 test.afterAll(async () => {
   await page.close();
 });
-
 test.beforeEach(async ({ page }) => {
   await page.goto(taskURL);
   await page.waitForLoadState("load");
 });
 
 test("are all images loaded properly?", async ({ page }) => {
-  await checkAndListImages(page);
-    expect(brokenImages).toEqual([]);
+  const brokenImages = checkImages(page);
+  const result = reportImages(brokenImages);
+  expect(brokenImages).toEqual([]);
 });
