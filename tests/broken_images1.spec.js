d661e445d90f344c96ebf3f178ef622106eb2ed3
LazzyBird <olgathek@gmail.com>
2024-03-05 18:01:46 +0200

    форматування результатів перевіряючих функцій у лог файл

--- tests/broken_images1.spec.js
+++ tests/broken_images1.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 import Env from "@helpers/env";
 import { checkImages, checkImgSrc } from "@datafactory/brokenImagesHelper";
-import { generateReportData, reportData } from "@helpers/reportHelper";
+// import { generateReportData, reportData } from "@helpers/reportHelper";
 const taskURL = Env.URL + "broken_images";
 let page;
 //
@@ -18,20 +18,10 @@ test.beforeEach(async ({ page }) => {
 
 test("Images are loaded properly", async ({ page }) => {
   const brokenImages = await checkImages(page);
-  const reportDataImages = generateReportData(brokenImages, checkImages);
-  reportData(reportDataImages);
-  ///expect(brokenImages).toEqual([]);
-  // для випадків коли биті зображення відомі, наприклад, 2 битка які зараз неважливі, то можна додати імпорт данних з відомими адресами, або кількістю битків:
-  expect(brokenImages.length).toEqual(2);
-  // або
-  //  expect(brokenImages).toContain("https://the-internet.herokuapp.com/asdf.jpg");
-  // expect(brokenImages).toContain("https://the-internet.herokuapp.com/hjkl.jpg");
-  // або імпорт з зовнішнього файла
-  // importedListOfBrokenImages = ["https://the-internet.herokuapp.com/asdf.jpg", "https://the-internet.herokuapp.com/hjkl.jpg"]
-  // expect(brokenImages).toEqual(importedListOfBrokenImages)
+  expect(brokenImages.length).toEqual(0);
+  // або якщо треба expect(brokenImages).toEqual(importedListOfBrokenImages)
 });
 test("Img.src return !404 response status code", async ({ page }) => {
   const imagesSrc = await checkImgSrc(page);
-  expect(imagesSrc.length).toEqual(2);// 2 on purpose because of 2 broken images
-  
+  expect(imagesSrc.length).toEqual(0);
 });
