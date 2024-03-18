e36687ab3566a26c88e9d563efd4a035ebd83b17
LazzyBird <olgathek@gmail.com>
2024-03-18 16:42:39 +0200

    eslint custom rules for literals check start

--- tests/digestAuthNOTCOMPLETE.spec.js
+++ tests/digestAuthNOTCOMPLETE.spec.js
@@ -4,24 +4,5 @@ import { fetchWithDigestAuth } from "@helpers/digestAuth";
 const taskURL = Env.URL + "digest_auth";
 test('cheesus', async () => {
   const correctAuth = await fetchWithDigestAuth(taskURL);
-  console.log(correctAuth);
-  // expect(correctAuth.status).toBe(200);
-})
-/*
-test("correct creds", async ({ request }) => {
-  const header1 = await request.get(`${taskURL}`);
-  console.log(header1);
+  expect(correctAuth.status).toBe(200);
 });
-test.beforeAll(async ({ browser }) => {
-  const context = await browser.newContext();
-  const page = await context.newPage();
-});
-
-test.afterAll(async () => {
-  await page.close();
-});
-
-test.beforeEach("Open URL", async ({ page }) => {
-  await page.goto(taskURL);
-});
-*/
