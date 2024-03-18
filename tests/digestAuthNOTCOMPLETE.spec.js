4f884a43be1de6562103bb982212278452e5a57f
LazzyBird <olgathek@gmail.com>
2024-03-18 15:53:45 +0200

    gone screaming - digest auth is solved

--- tests/digestAuthNOTCOMPLETE.spec.js
+++ tests/digestAuthNOTCOMPLETE.spec.js
@@ -2,14 +2,16 @@ import { test, expect } from "@playwright/test";
 import Env from "@helpers/env";
 import { fetchWithDigestAuth } from "@helpers/digestAuth";
 const taskURL = Env.URL + "digest_auth";
-
-test("correct creds", async () => {
+test('cheesus', async () => {
   const correctAuth = await fetchWithDigestAuth(taskURL);
   console.log(correctAuth);
   // expect(correctAuth.status).toBe(200);
-});
-
+})
 /*
+test("correct creds", async ({ request }) => {
+  const header1 = await request.get(`${taskURL}`);
+  console.log(header1);
+});
 test.beforeAll(async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
