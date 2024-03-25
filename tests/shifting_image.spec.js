96519780c8890c5d9c5f8b8a0d12abe9ea6c063f
LazzyBird <olgathek@gmail.com>
2024-03-25 17:00:40 +0200

    чортіщо з тим bounding box

--- tests/shifting_image.spec.js
+++ tests/shifting_image.spec.js
@@ -1,12 +1,5 @@
 import { test, expect } from "@playwright/test";
-import Env from "@helpers/env"
-const mainURL = Env.URL +
-  "shifting_content/image";
-const randomShift = mainURL + "?mode=random";
-const ramdomPixels = mainURL + "?pixel_shift=100" // ! якщо можна задати зсув у запиті, чому б не додати рандомайзер й функцію для перевірки чи відповідає зсув параметру
-const randomBoth = mainURL + "?mode=random&pixel_shift=100";
-const simpleAppend = mainURL + "?image_type=simple";
-
+import { taskURL } from "@data_assets/shiftingImageURL"
 let page;
 
 test.beforeAll(async ({ browser }) => {
@@ -17,11 +10,35 @@ test.afterAll(async () => {
   await page.close();
 });
 
-test.beforeEach(async ({ page }) => {
-  await page.goto(taskURL);
-  await page.waitForLoadState("load");
-});
+async function boundingBoxGetAndCompare(page, url, container, content) {
+  await page.goto(url);
+  const image = await page.locator(container);
+  const div = await page.locator(content);
+
+  const imageBounds = await image.boundingBox();
+  const divBounds = await div.boundingBox();
+
+  if (!imageBounds || !divBounds) {
+    throw new Error('Bounding box not found');
+  }
+
+  const { x: imageX, y: imageY, width: imageWidth, height: imageHeight } = imageBounds;
+  const { x: divX, y: divY, width: divWidth, height: divHeight } = divBounds;
+
+  const a = expect(imageX).toBeGreaterThanOrEqual(divX);
+  const b = expect(imageY).toBeGreaterThanOrEqual(divY);
+  const c = expect(imageX + imageWidth).toBeLessThanOrEqual(divX + divWidth);
+  const d = expect(imageY + imageHeight).toBeLessThanOrEqual(divY + divHeight);
+  const results = await Promise.all([a, b, c, d]);
+  return results.every((res) => res.passed)
+};
+
+test('Image does not intersect its container bounding box', async ({ page }) => {
+  const result = await boundingBoxGetAndCompare(page, taskURL.ramdomPixels, '#content', '#content > div > img');
+  expect(result).toBeTruthy();
+})
 //! все не подобається, аж трясе, тупізм якийсь написала
+/*
 test("Image has proper position", async ({ page }) => {
   try {
     const imageElement = page.locator("#content > div");
@@ -36,3 +53,4 @@ test("Image has proper position", async ({ page }) => {
     throw error;
   }
 });
+*/
