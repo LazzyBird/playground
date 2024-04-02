70cb9d0769d4a5554aa041a748988a2aec51368e
LazzyBird <olgathek@gmail.com>
2024-04-02 14:58:26 +0300

    божепамажі

--- tests/shiftingContentMenu_REWRITE.spec.js
+++ tests/shiftingContentMenu_REWRITE.spec.js
@@ -1,39 +1,50 @@
 import { test, expect } from "@playwright/test";
 import { taskURL } from "@data_assets/shiftingContent";
+import { menuItems } from "@data_assets/menuItems";
 
 test("Random mode menu items", async ({ page }) => {
-  await page.goto(taskURL.random);
-//? дописати
-});
-test("Shift mode menu items", async ({ page }) => {
-  await page.goto(taskURL.shift);
-  //? дописати
-});
-test("Both modes menu items", async ({ page }) => {
   await page.goto(taskURL.randomAndShift);
-  //? дописати
-});
+  let counter = 0;
+  let errorCount = 0;
+  while (counter < 10) {
+    try {
+      for (let i = 0; i < menuItems.length; i++) {
+        const a = await expect(page.getByText(menuItems[i])).toHaveScreenshot(`menuItems${i}.png`);
+        if (!a) {
+          await page.getByText(menuItems[i]).screenshot({ path: `menuItems${i}_failed.png` });
+          errorCount++;
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    counter++;
+    await page.reload();
+  }
+  expect(errorCount).toBe(0)
+}
+);
 
- /* //? кандидат на випиляння
+/* //? кандидат на випиляння
 test.describe("Every menu item is according to layout ", () => {
-  test("Home menu item is according to layout ", async ({ page }) => {
-    await expect(page.getByText("Home")).toHaveScreenshot();
-  });
+ test("Home menu item is according to layout ", async ({ page }) => {
+   await expect(page.getByText("Home")).toHaveScreenshot();
+ });
 
-  test("About menu item is according to layout ", async ({ page }) => {
-    await expect(page.getByText("About")).toHaveScreenshot();
-  });
+ test("About menu item is according to layout ", async ({ page }) => {
+   await expect(page.getByText("About")).toHaveScreenshot();
+ });
 
-  test("Contact Us menu item is according to layout ", async ({ page }) => {
-    await expect(page.getByText("Contact Us")).toHaveScreenshot();
-  });
+ test("Contact Us menu item is according to layout ", async ({ page }) => {
+   await expect(page.getByText("Contact Us")).toHaveScreenshot();
+ });
 
-  test("Portfolio menu item is according to layout ", async ({ page }) => {
-    await expect(page.getByText("Portfolio")).toHaveScreenshot();
-  });
+ test("Portfolio menu item is according to layout ", async ({ page }) => {
+   await expect(page.getByText("Portfolio")).toHaveScreenshot();
+ });
 
-  test("Gallery menu item is according to layout ", async ({ page }) => {
-    await expect(page.getByText("Gallery")).toHaveScreenshot();
-  });
+ test("Gallery menu item is according to layout ", async ({ page }) => {
+   await expect(page.getByText("Gallery")).toHaveScreenshot();
+ });
 });
- */
+*/
--- tests/typos_paragraph.spec.js
+++ tests/typos_paragraph.spec.js
@@ -1,8 +1,10 @@
+import Env from "@helpers/env";
 import { test, expect } from "@playwright/test";
 import { typoCounter } from "@datafactory/typosHelper";
+const taskURL = Env.URL + "typos";
 
 let expectedText = `Sometimes you'll see a typo, other times you won't.`;
 test(`typo counter`, async ({ page }) => {
-  const typoCount = await typoCounter(page, 'Sometimes', expectedText, 10);
+  const typoCount = await typoCounter(page, taskURL, 'Sometimes', expectedText, 10);
   expect(typoCount).toEqual(0);
 })
