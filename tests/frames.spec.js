07082c3f4870bb298988fb58955b0725aefac835
LazzyBird <olgathek@gmail.com>
2024-04-17 16:10:02 +0300

    nested frames by urls

--- tests/frames.spec.js
+++ tests/frames.spec.js
@@ -3,13 +3,13 @@ import Env from '@helpers/env';
 
 const nestedFramesURL = Env.URL + "nested_frames";
 const iframeURL = Env.URL + "iframe";
-const nestedFrNames = {
-    leftFrame: "LEFT",
-    middleFrame: "MIDDLE",
-    rightFrame: "RIGHT",
-    bottomFrame: "BOTTOM"
+const nestedFrames = {
+    frame_left: "LEFT",
+    frame_middle: "MIDDLE",
+    frame_right: "RIGHT",
+    frame_bottom: "BOTTOM"
 };
-const namesArray = Object.values(nestedFrNames);
+const f_url = Object.keys(nestedFrames);
 //? мама мія хір уі го егейн май май хау кен ай резіст ю
 //? що тут перевіряти?
 test('Nested Frames all 4 are visible by screenshot', async ({ page }) => {
@@ -17,8 +17,19 @@ test('Nested Frames all 4 are visible by screenshot', async ({ page }) => {
     await page.waitForLoadState();
     await expect(page).toHaveScreenshot();
 });
-test('nested frames by text', async ({ page }) => {
-    
-})
+test('left frame content by its url', async ({ page }) => {
+    await page.goto(`https://the-internet.herokuapp.com/frame_left`);
+    await page.waitForLoadState();
+    expect(page.locator('body')).toHaveText(nestedFrames.leftFrame);
+});
+test('frame by frameLocator', async ({ page }) => {
+    for (const key of f_url) {
+        await page.goto(Env.URL + key);
+        await page.waitForLoadState();
+        const text = await page.locator('body').innerText();
+        await expect(page.locator('body')).toHaveText(nestedFrames[`${key}`]);
+        console.log(text);
+    }
+});
 //? порівняти з скріншотом? чи треба перевіряти функціонал? там є ще перевірка такого редактора
 test(`iFrame with TinyMCE editor`, async ({ page }) => { });
