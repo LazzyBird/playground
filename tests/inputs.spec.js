a66210cb765ba6d88e609c8e193460b567debe32
LazzyBird <olgathek@gmail.com>
2024-04-22 17:01:29 +0300

    input monkey works

--- tests/inputs.spec.js
+++ tests/inputs.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 import Env from "@helpers/env";
 import Chance from "chance";
-import { InputHandler } from "@helpers/inputHandler";
+//import InputHandler from "@helpers/inputHandler";
 const chance = new Chance;
 
 test.beforeEach(async ({ page }) => {
@@ -18,7 +18,44 @@ test.describe('fill form with different inputs', async () => {
         const b = await page.locator('input').inputValue();
         expect(parseInt(b)).toEqual(a);
     });
+    test('fill with random floating', async ({ page }) => {
+        const a = chance.floating();
+        await page.locator('input').fill(a.toString());
+        const b = await page.locator('input').inputValue();
+        expect(parseFloat(b)).toEqual(a);
+    })
 });
+test.describe('fill and change with arrow buttons', async () => {
+    test('only arrow buttons', async ({ page }) => {
+        const expectedResult = await randomUpDownKeys(page, 'input');
+        const actualResult = await page.locator('input').inputValue();
+        console.log(`Expected result: ${expectedResult}\nActual result: ${actualResult}`);
+        expect(parseInt(actualResult)).toEqual(expectedResult);
+    })
+});
+async function randomUpDownKeys(page, loc) {
+    await page.locator(loc).focus();
+    let repeat = chance.integer({ min: 50, max: 500 }), upN = 0, downN = 0, result = 0;
+    console.info('N of keypresses: ', repeat);
+    while (repeat) {
+        const whichKey = chance.bool();
+        if (whichKey) {
+            await page.keyboard.press('ArrowUp');
+            result++;
+            upN++;
+        } else {
+            await page.keyboard.press('ArrowDown');
+            result--;
+            downN++;
+        }
+        repeat--;
+    };
+    const total = upN + downN;
+    const difference = upN - downN;
+    console.info(`Up pressed ${upN} times, Down pressed ${downN} times, total ${total}, difference ${difference}`);
+    return result;
+}
+/*  //* щось не процює цей клас, треба подумати
 test.describe('fill form with different inputs with Class', async ({ page }) => {
     const inputHandler = new InputHandler(page);
 
@@ -31,3 +68,4 @@ test.describe('fill form with different inputs with Class', async ({ page }) => 
 
     // Аналогічно можна викликати інші методи fillWithFloating, fillWithMixed та використовувати відповідні методи parse
 });
+ */
