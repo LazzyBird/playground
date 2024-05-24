76bbc1f90fbead7b9005648e7adb69b5ed0a7cd7
LazzyBird <olgathek@gmail.com>
2024-05-24 11:31:45 +0300

    00

--- JSE2/module3.js
+++ JSE2/module3.js
@@ -18,6 +18,13 @@
 
     let almostEmptyObject = new AlmostEmptyClass(120); // 120 
     almostEmptyObject.sayHi(); // -> Hi! 
-    almostEmptyObject.sayHello(); // error Uncaught TypeError: almostEmptyObject.sayHello is not a function
-    AlmostEmptyClass.sayHello(); // -> Hello! - #брехня бо нічого не пише
+    almostEmptyObject.sayHello();
+    // error Uncaught TypeError: almostEmptyObject.sayHello is not a function
+    AlmostEmptyClass.sayHello(); // -> Hello! - буде працювати якщо закоментить рядок що викликає помилку - бо так JS й працює
+}
+{ //? при запуску коду в пісочниці курсу вилазить рекомендація не використовувати Number в якості конструктора
+    let n = new Number(100.123);
+    let fixed = n.toFixed(2);
+    let test1 = Number.isInteger(100);
+    let test2 = n.isInteger(100); // -> n.isInteger is not a function
 }
